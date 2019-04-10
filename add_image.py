94ec2a419e5ffec0829a9a37bc6766c108fce95c
Victor STEYAERT <40288838+Nqsir@users.noreply.github.com>
2019-04-10 17:46:06 +0200

    Delete add_image.py

--- add_image.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-from shutil import copy2
-
-
-if __name__ == '__main__':
-    mpl_path = os.path.join(sys.path[-1], os.path.join('matplotlib', os.path.join('mpl-data', 'images')))
-    copy2('name.png', mpl_path)
-    copy2('name_large.png', mpl_path)
